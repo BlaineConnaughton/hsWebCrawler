807f8cc8f6a8d26c5b5e428faf0ca35343135947
bconnaughton <bconnaughton@hubspot.com>
2014-02-28 17:27:25 -0500

    Latest updates, Attempts to control the massive amounts of post requests only semi successful

--- main.py
+++ main.py
@@ -15,15 +15,25 @@
 # limitations under the License.
 #
 import webapp2
+import logging
+import json
+import time
 from google.appengine.api import memcache
 from google.appengine.api import urlfetch
 from google.appengine.api import taskqueue
 from bs4 import BeautifulSoup as bs
 from google.appengine.ext import db
 
+
 class SessionScraped(db.Model):
     ran_on = db.DateTimeProperty(required=True,auto_now_add=True)
-    domainurl = db.StringProperty(required=True)
+    domainurl = db.StringProperty()
+
+    @staticmethod
+    def get_all(count=None):
+        q = SessionScraped.all()
+        data = q.fetch(count)
+        return data
 
 
 class ScrapedUrl(db.Model):
@@ -37,54 +47,82 @@ class ScrapedUrl(db.Model):
     def created(self):
         return format_date(self.ran_on)
 
-    def scrapedurlgetall():
+    @staticmethod
+    def get_all(count=None):
         q = ScrapedUrl.all()
-        data = q.fetch(10)
+        data = q.fetch(count)
+        return data
 
-def get_urls(url , domain):
+def get_urls(url , mysession):
 
     data = memcache.get(url)
     #if we haven't seen this url before, add it then go do processing, if we have, exit
     if data is None:
         memcache.add(url, url, 3600)
+        logging.warning("Seen this url before  :  " + url)
     else:
         return
 
-    response = urlfetch.get(url)
+    response = urlfetch.fetch(url)
 
-    soup = bs.BeautifulSoup(response.content)
+    soup = bs(response.content)
 
     for link in soup.findAll('a'):
         #check to see if the links are on the domain we started with, if they are not, probably don't need to analyze
         domain_check = str(link)
-
         #if data is None and it passes domain check, add it to the task queue
-        if data == None and domain_check.find(domain) != -1:
+        if domain_check.find(str(mysession.domainurl)) != -1:
+            #double check that we haven't already scraped this from a different queue
+            data = memcache.get(link.get('href'))
+            if data == None:
+                #update table
+                db = ScrapedUrl(domainurl=mysession.domainurl , url=link.get('href'))
+                db.session = mysession
+                db.put()
 
+                #queue api point to make this request again
+                params={ 'url': link.get('href') , 'session': mysession.key().id()}
+                logging.warning("params :  " + str(params))
+                taskqueue.add(url='/api', params=params)
 
-            #update table
-            db = ScrapedUrl()
-            db.url = link
-            db.domainurl = domain
-            #db.session = mysession
-            db.put()
+    return
 
-            #create api point to make this request again
-            taskqueue.add(url='/api/approval/publish', params={ 'url': link.get('href') , 'domain': domain})
+class TestHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write('<h1>Your request</h1>')
+        self.response.out.write(self.request)
+        self.response.out.write('<h2>Your q value</h2>')
+        self.response.out.write(self.request.get('q'))
 
-    return crawled
+class GetURLHandler(webapp2.RequestHandler):
+    def post(self):
+        url = self.request.get('url')
+        sessionid = self.request.get('session')
+
+        session = SessionScraped.get_by_id(int(sessionid))
+
+        get_urls(url=url , mysession=session)
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write("Scrape this endpoint")
 
     def post(self):
         url = self.request.get('url')
         domain = self.request.get('domain')
-        get_urls(url=url , domain=domain)
-        return 'Eagle has landed'
+
+        session = SessionScraped()
+        session.domainurl = domain
+        session.put()
+
+        get_urls(url=url , mysession=session)
 
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler) , ('/api', GetURLHandler) ,  ('/test', TestHandler)
 ], debug=True)
+
+
+
+
